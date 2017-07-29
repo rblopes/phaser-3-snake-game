0a61c863bd8e9117951eef03f416ce4a90452fbf
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2017-07-29 15:40:34 -0300

    Reverting some changes made earlier.

--- app/scripts/scenes/game.js
+++ app/scripts/scenes/game.js
@@ -18,22 +18,22 @@ export default class Game extends Phaser.Scene {
 
   create(/* data */) {
     this.scene.launch('Maze');
-    // this.scene.launch('Scoreboard');
+    this.scene.launch('Scoreboard');
 
     const frame = this.add.image(0, 32, 'frame');
     frame.setOrigin(0, 0);
 
     const maze = this.scene.manager.getScene('Maze');
-    // const scoreboard = this.scene.manager.getScene('Scoreboard');
+    const scoreboard = this.scene.manager.getScene('Scoreboard');
 
     //  When a food gets eaten, update the score.
-    // maze.events.on('FOOD_EATEN', () => {
-    //   scoreboard.scorePoint();
-    // });
+    maze.events.on('FOOD_EATEN', () => {
+      scoreboard.scorePoint();
+    });
 
     //  When game is over, tell the player.
-    // maze.events.on('SNAKE_DEAD', () => {
-    //   scoreboard.showGameOver();
-    // });
+    maze.events.on('SNAKE_DEAD', () => {
+      scoreboard.showGameOver();
+    });
   }
 }
