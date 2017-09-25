717135223440a1c2be27fcbe13817db09176bb68
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2017-09-25 12:38:39 -0300

    Another few code tweaks.

--- app/scripts/scenes/game.js
+++ app/scripts/scenes/game.js
@@ -8,32 +8,24 @@
 export default class Game extends Phaser.Scene {
   preload() {
     this.load.path = 'assets/';
-
-    this.load.image('food');
-    this.load.image('body');
-    this.load.image('frame');
-    this.load.image('numerals');
-    this.load.image('game-over');
+    this.load.image(['food', 'body', 'frame', 'numerals', 'game-over']);
   }
 
   create(/* data */) {
-    this.scene.launch('Maze');
-    this.scene.launch('Scoreboard');
+    const maze = this.scene.manager.getScene('Maze');
+    const scoreboard = this.scene.manager.getScene('Scoreboard');
 
     const frame = this.add.image(0, 32, 'frame');
     frame.setOrigin(0, 0);
 
-    const maze = this.scene.manager.getScene('Maze');
-    const scoreboard = this.scene.manager.getScene('Scoreboard');
+    this.scene
+      .launch('Maze')
+      .launch('Scoreboard');
 
     //  When a food gets eaten, update the score.
-    maze.events.on('FOOD_EATEN', () => {
-      scoreboard.scorePoint();
-    });
+    maze.events.on('FOOD_EATEN', () => scoreboard.scorePoint());
 
     //  When game is over, tell the player.
-    maze.events.on('SNAKE_DEAD', () => {
-      scoreboard.showGameOver();
-    });
+    maze.events.on('SNAKE_DEAD', () => scoreboard.showGameOver());
   }
 }
