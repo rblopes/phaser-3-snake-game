19f8bf4cc5e2d1b029b1695d0df9f71bc17d00ac
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2017-09-25 12:32:59 -0300

    Reordering scene loading
    I wonder if this will run into race condition issues later.

--- app/scripts/scenes/index.js
+++ app/scripts/scenes/index.js
@@ -5,6 +5,6 @@
  * Declares all present game states.
  */
 
+export {default as Game} from './game';
 export {default as Maze} from './maze';
 export {default as Scoreboard} from './scoreboard';
-export {default as Game} from './game';
