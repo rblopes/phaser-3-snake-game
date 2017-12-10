48f821766230c7384ee39cf14f0b709eb88acd3f
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2017-12-10 13:17:29 -0200

    Fixed Snake entity crashing due to API changes.

--- app/scripts/objects/snake.js
+++ app/scripts/objects/snake.js
@@ -86,7 +86,8 @@ export default class Snake {
 
     //  Update the body segments and place the last coordinate into
     //  `this.tailPosition`.
-    this.body.shiftPosition(
+    Phaser.Actions.ShiftPosition(
+      this.body.children.entries,
       this.headPosition.x * 16,
       this.headPosition.y * 16,
       1,
@@ -95,7 +96,11 @@ export default class Snake {
 
     //  Check to see if any of the body pieces have the same x/y as the head.
     //  If they do, the head ran into the body.
-    let hitBody = this.body.getFirst({x: this.head.x, y: this.head.y}, 1);
+    let hitBody = Phaser.Actions.GetFirst(
+      this.body.children.entries,
+      {x: this.head.x, y: this.head.y},
+      1
+    );
 
     if (hitBody) {
       //  Game Over!
