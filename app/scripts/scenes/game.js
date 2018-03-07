1f56b885f8c83b8123bd9089fd2d1b11aba3f66e
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2018-03-07 18:16:08 -0300

    Moved game logic to host scene.
    Now I can remove that useless `Food#eat()` method, sure.

--- app/scripts/scenes/game.js
+++ app/scripts/scenes/game.js
@@ -19,9 +19,13 @@ export default class Game extends Phaser.Scene {
       fontConfig.image,
       Phaser.GameObjects.BitmapText.ParseRetroFont(this, fontConfig)
     );
+
+    //  Game points.
+    this.points = 0;
   }
 
   create(/* data */) {
+    //  Put the frame behind the maze.
     this.add.image(0, 0, 'frame').setOrigin(0, 0);
 
     //  Get a reference of the scenes to start.
@@ -30,13 +34,78 @@ export default class Game extends Phaser.Scene {
 
     //  Run both scenes in parallel.
     this.scene
-      .launch(scoreboard)
+      .launch(scoreboard, {gameScene: this})
       .launch(maze);
 
-    //  When a food gets eaten, update the score.
-    maze.events.on('FOOD_EATEN', () => scoreboard.scorePoint());
+    //  Add the game objects to the maze scene.
+    this.food = maze.addFood(3, 4);
+    this.snake = maze.addSnake(8, 8);
 
-    //  When game is over, tell the player.
-    maze.events.on('SNAKE_DEAD', () => scoreboard.showGameOver());
+    //  Create our keyboard controls.
+    this.cursors = this.input.keyboard.addKeys({
+      leftKey: Phaser.Input.Keyboard.KeyCodes.LEFT,
+      rightKey: Phaser.Input.Keyboard.KeyCodes.RIGHT
+    });
+
+    //  Count how long a key is being pressed down.
+    this.keyDownCounter = 0;
+  }
+
+  update(time) {
+    if (this.snake.alive) {
+      this.updateInput();
+      this.updateLogic(time);
+    }
+  }
+
+  //  ------------------------------------------------------------------------
+
+  updateInput() {
+    const {leftKey, rightKey} = this.cursors;
+
+    //  NOTE: Phaser still lacks a reliable method for checking when a key was
+    //  just pressed. Our last resort is to update a counter for the duration
+    //  of that key press and, based on its value, decide whether to change
+    //  the snake direction or not on the next branching logic.
+    if (leftKey.isDown || rightKey.isDown) {
+      this.keyDownCounter += 1;
+    }
+    else {
+      this.keyDownCounter = 0;
+    }
+
+    //  Check which key is pressed, then change the direction the snake is
+    //  heading.
+    if (leftKey.isDown && this.keyDownCounter < 2) {
+      this.snake.turnLeft();
+    }
+    else if (rightKey.isDown && this.keyDownCounter < 2) {
+      this.snake.turnRight();
+    }
+  }
+
+  updateLogic(time) {
+    const {food, snake} = this;
+
+    if (snake.update(time)) {
+      //  If the snake updated, we need to check for collision against food.
+      if (snake.collideWithFood(food, this.points)) {
+        this.updatePoints();
+        food.reposition(snake);
+      }
+    }
+
+    if (!snake.alive) {
+      this.endGame();
+    }
+  }
+
+  endGame() {
+    this.events.emit('snake-died');
+  }
+
+  updatePoints() {
+    this.points += 5;
+    this.events.emit('food-eaten', this.points);
   }
 }
--- app/scripts/scenes/maze.js
+++ app/scripts/scenes/maze.js
@@ -25,57 +25,13 @@ export default class Maze extends Phaser.Scene {
     });
   }
 
-  create(/* data */) {
-    //  Add the game objects to the scene.
-    this.food = new Food(this, 3, 4);
-    this.snake = new Snake(this, 8, 8);
+  //  ------------------------------------------------------------------------
 
-    //  Create our keyboard controls.
-    this.cursors = this.input.keyboard.addKeys({
-      leftKey: Phaser.Input.Keyboard.KeyCodes.LEFT,
-      rightKey: Phaser.Input.Keyboard.KeyCodes.RIGHT
-    });
-
-    //  Count how long a key is being pressed down.
-    this.keyDownCounter = 0;
+  addFood(x = 0, y = x) {
+    return new Food(this, x, y);
   }
 
-  update(time/* , delta */) {
-    const {food, snake} = this;
-    const {leftKey, rightKey} = this.cursors;
-
-    if (!snake.alive) {
-      this.events.emit('SNAKE_DEAD');
-      this.scene.pause();
-      return;
-    }
-
-    //  NOTE: Phaser still lacks a reliable method for checking when a key was
-    //  just pressed. Our last resort is to update a counter for the duration
-    //  of that key press and, based on its value, decide whether to change
-    //  the snake direction or not on the next branching logic.
-    if (leftKey.isDown || rightKey.isDown) {
-      this.keyDownCounter += 1;
-    }
-    else {
-      this.keyDownCounter = 0;
-    }
-
-    //  Check which key is pressed, and then change the direction the snake is
-    //  heading.
-    if (leftKey.isDown && this.keyDownCounter < 2) {
-      snake.turnLeft();
-    }
-    else if (rightKey.isDown && this.keyDownCounter < 2) {
-      snake.turnRight();
-    }
-
-    if (snake.update(time)) {
-      //  If the snake updated, we need to check for collision against food.
-      if (snake.collideWithFood(food)) {
-        this.events.emit('FOOD_EATEN');
-        food.reposition(snake);
-      }
-    }
+  addSnake(x = 0, y = x) {
+    return new Snake(this, x, y);
   }
 }
