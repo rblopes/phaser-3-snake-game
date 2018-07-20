e3b823055486f2096e7cf8109162c237fe4c8977
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2018-07-20 15:56:59 -0300

    Fixed bitmap font parameters.

--- app/scripts/constants/bitmap-fonts.js
+++ app/scripts/constants/bitmap-fonts.js
@@ -7,10 +7,10 @@
 
 export default {
   image: 'font',
-  width: 14,
-  height: 14,
+  width: 16,
+  height: 16,
   chars: '0123456789   ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  offset: {x: 1, y: 1},
-  spacing: {x: 2, y: 2},
+  offset: {x: 0, y: 0},
+  spacing: {x: 0, y: 0},
   charsPerRow: 13
 };
